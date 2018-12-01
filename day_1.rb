c301136ca22dba330e9a7b448ee5fa3f8db505bf
Katherine Wu <kwugirl@gmail.com>
2018-12-01 15:33:14 -0500

    1A

---
+++ day_1.rb
@@ -0,0 +1,8 @@
+class Calibrator
+  def frequency(changes, starting_point: 0)
+    starting_point + changes.sum
+  end
+end
+
+input = File.readlines('day_1_input.txt').map { |line| line.strip.to_i }
+puts Calibrator.new.frequency(input)
---
+++ tests/day_1_tests.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require_relative '../day_1'
+
+class Day1Test < Minitest::Test
+  def test_frequency
+    examples = [
+      {
+        input: [+1, -2, +3, +1],
+        output: 3
+      },
+      {
+        input: [+1, +1, +1],
+        output: 3
+      },
+      {
+        input: [+1, +1, -2],
+        output: 0
+      },
+      {
+        input: [-1, -2, -3],
+        output: -6
+      }
+    ]
+
+    examples.each do |example|
+      assert_equal example[:output], Calibrator.new.frequency(example[:input])
+    end
+  end
+end
