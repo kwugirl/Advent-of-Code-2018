b55654255bc0361ca64e6e821c1dd1ff5f0feb32
Katherine Wu <kwugirl@gmail.com>
2018-12-01 19:49:02 -0500

    1B

--- day_1.rb
+++ day_1.rb
@@ -2,7 +2,28 @@ class Calibrator
   def total_frequency(changes, starting_point: 0)
     starting_point + changes.sum
   end
+
+  def frequency(current, change)
+    current + change
+  end
+
+  def find_repeated_frequency(changes, starting_point: 0)
+    observed_frequencies = [starting_point]
+
+    while true
+      changes.each do |change|
+        new_frequency = frequency(observed_frequencies.last, change)
+
+        if observed_frequencies.include?(new_frequency)
+          return new_frequency
+        else
+          observed_frequencies << new_frequency
+        end
+      end
+    end
+  end
 end
 
 # input = File.readlines('day_1_input.txt').map { |line| line.strip.to_i }
 # puts Calibrator.new.total_frequency(input)
+# puts Calibrator.new.find_repeated_frequency(input)
--- tests/day_1_tests.rb
+++ tests/day_1_tests.rb
@@ -26,4 +26,33 @@ class Day1Test < Minitest::Test
       assert_equal example[:output], Calibrator.new.total_frequency(example[:input])
     end
   end
+
+  def test_find_repeated_frequency
+    examples = [
+      {
+        input: [+1, -2, +3, +1],
+        output: 2
+      },
+      {
+        input: [+1, -1],
+        output: 0
+      },
+      {
+        input: [+3, +3, +4, -2, -4],
+        output: 10
+      },
+      {
+        input: [-6, +3, +8, +5, -6],
+        output: 5
+      },
+      {
+        input: [+7, +7, -2, -7, -4],
+        output: 14
+      }
+    ]
+
+    examples.each do |example|
+      assert_equal example[:output], Calibrator.new.find_repeated_frequency(example[:input])
+    end
+  end
 end
