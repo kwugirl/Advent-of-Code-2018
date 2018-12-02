70c035cc6cf160dc37d75d5c9e75f6fb6e398386
Katherine Wu <kwugirl@gmail.com>
2018-12-01 20:37:42 -0500

    Benchmark using hash for observed_frequencies

--- day_1.rb
+++ day_1.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 class Calibrator
   def total_frequency(changes, starting_point: 0)
     starting_point + changes.sum
@@ -22,8 +24,42 @@ class Calibrator
       end
     end
   end
+
+  def find_repeated_frequency_alt(changes, starting_point: 0)
+    observed_frequencies = { starting_point => true }
+    current = starting_point
+
+    while true
+      changes.each do |change|
+        new_frequency = frequency(current, change)
+
+        if observed_frequencies[new_frequency]
+          return new_frequency
+        else
+          observed_frequencies[new_frequency] = true
+          current = new_frequency
+        end
+      end
+    end
+  end
 end
 
-# input = File.readlines('day_1_input.txt').map { |line| line.strip.to_i }
+input = File.readlines('day_1_input.txt').map { |line| line.strip.to_i }
 # puts Calibrator.new.total_frequency(input)
 # puts Calibrator.new.find_repeated_frequency(input)
+puts Calibrator.new.find_repeated_frequency_alt(input)
+
+# Benchmark.bm(25) do |x|
+#   x.report("observed_frequencies as array:") do
+#     Calibrator.new.find_repeated_frequency(input)
+#   end
+#   x.report("observed_frequencies as hash:") do
+#     Calibrator.new.find_repeated_frequency_alt(input)
+#   end
+# end
+
+##### Results
+#                                 user     system      total        real
+# observed_frequencies as array: 56.971103   0.194353  57.165456 ( 57.520675)
+# observed_frequencies as hash:  0.032629   0.004284   0.036913 (  0.037067)
+
--- tests/day_1_tests.rb
+++ tests/day_1_tests.rb
@@ -55,4 +55,33 @@ class Day1Test < Minitest::Test
       assert_equal example[:output], Calibrator.new.find_repeated_frequency(example[:input])
     end
   end
+
+  def test_find_repeated_frequency_alt
+    examples = [
+      {
+        input: [+1, -2, +3, +1],
+        output: 2
+      },
+      {
+        input: [+1, -1],
+        output: 0
+      },
+      {
+        input: [+3, +3, +4, -2, -4],
+        output: 10
+      },
+      {
+        input: [-6, +3, +8, +5, -6],
+        output: 5
+      },
+      {
+        input: [+7, +7, -2, -7, -4],
+        output: 14
+      }
+    ]
+
+    examples.each do |example|
+      assert_equal example[:output], Calibrator.new.find_repeated_frequency_alt(example[:input])
+    end
+  end
 end
