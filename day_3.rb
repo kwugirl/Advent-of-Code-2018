6606f1896fb65a7048f51eda15e4e2e43b5aa9bc
Katherine Wu <kwugirl@gmail.com>
2018-12-03 20:01:29 -0500

    3A: Add calculation of occupied coordinates

--- day_3.rb
+++ day_3.rb
@@ -1,5 +1,5 @@
 class Claim
-  attr_reader :id, :pos_from_left, :pos_from_top, :width, :height
+  attr_reader :id, :pos_from_left, :pos_from_top, :width, :height, :occupied_coordinates
 
   def initialize(claim)
     parsed = /^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$/.match(claim)
@@ -9,5 +9,22 @@ class Claim
     @pos_from_top = parsed[3].to_i
     @width = parsed[4].to_i
     @height = parsed[5].to_i
+  end
+
+  def occupied_coordinates
+    @occupied_coordinates ||= begin
+      coordinates = {}
+      starting_x = pos_from_left + 1
+      ending_x = pos_from_left + width
+      starting_y = pos_from_top + 1
+      ending_y = pos_from_top + height
+
+      (starting_x..ending_x).each do |x|
+        (starting_y..ending_y).each do |y|
+          coordinates["(#{x},#{y})"] = 1
+        end
+      end
+      coordinates
+    end
   end
 end
--- tests/day_3_tests.rb
+++ tests/day_3_tests.rb
@@ -13,4 +13,18 @@ class Day3Test < Minitest::Test
     assert_equal 5, claim.width
     assert_equal 4, claim.height
   end
+
+  def test_occupied_coordinates
+    expected = {
+      "(4,3)"=>1, "(4,4)"=>1, "(4,5)"=>1, "(4,6)"=>1,
+      "(5,3)"=>1, "(5,4)"=>1, "(5,5)"=>1, "(5,6)"=>1,
+      "(6,3)"=>1, "(6,4)"=>1, "(6,5)"=>1, "(6,6)"=>1,
+      "(7,3)"=>1, "(7,4)"=>1, "(7,5)"=>1, "(7,6)"=>1,
+      "(8,3)"=>1, "(8,4)"=>1, "(8,5)"=>1, "(8,6)"=>1
+    }
+
+    claim = Claim.new("#123 @ 3,2: 5x4")
+
+    assert_equal expected, claim.occupied_coordinates
+  end
 end
