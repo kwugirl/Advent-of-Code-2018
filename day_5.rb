6b08a02aa1d8bbb0984e339cf50fc0a839ab52b5
Katherine Wu <kwugirl@gmail.com>
2018-12-05 11:52:26 -0500

    5A: Add iterative approach

--- day_5.rb
+++ day_5.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 POLAR_PAIRS = begin
   pairs = Hash.new(false)
   ('a'..'z').each do |letter|
@@ -23,15 +25,42 @@ def polar_pair?(pair)
   POLAR_PAIRS[pair]
 end
 
-def resolve_polarity(polymer)
+def resolve_polarity_recursive(polymer)
   reduced_polymer = remove_polar_pairs(polymer)
 
   if reduced_polymer == polymer
     return reduced_polymer
   else
-    resolve_polarity(reduced_polymer)
+    resolve_polarity_recursive(reduced_polymer)
   end
 end
 
+def resolve_polarity_iterative(polymer)
+  reduced_polymers = {
+    current: polymer,
+    reduced: remove_polar_pairs(polymer)
+  }
+
+  while reduced_polymers[:current] != reduced_polymers[:reduced]
+    reduced_polymers[:current] = reduced_polymers[:reduced]
+    reduced_polymers[:reduced] = remove_polar_pairs(reduced_polymers[:reduced])
+  end
+
+  reduced_polymers[:current]
+end
+
 input = File.read('inputs/day_5.txt').strip
-puts resolve_polarity(input).length
+puts resolve_polarity_iterative(input).length
+
+# Benchmark.bm(18) do |x|
+#   x.report("recursive approach") do
+#     resolve_polarity_recursive(input)
+#   end
+#   x.report("iterative approach") do
+#     resolve_polarity_iterative(input)
+#   end
+# end
+# # RESULTS
+#                          user     system      total        real
+# recursive approach  16.282709   0.150932  16.433641 ( 16.459125)
+# iterative approach  16.017317   0.110487  16.127804 ( 16.141921)
--- tests/day_5_tests.rb
+++ tests/day_5_tests.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require_relative '../day_5'
 
 class Day5Test < Minitest::Test
-  def test_resolve_polarity
+  def test_resolve_polarity_recursive
     examples = [
       {
         input: "aA",
@@ -27,7 +27,36 @@ class Day5Test < Minitest::Test
     ]
 
     examples.each do |example|
-      assert_equal example[:expected], resolve_polarity(example[:input])
+      assert_equal example[:expected], resolve_polarity_recursive(example[:input])
+    end
+  end
+
+  def test_resolve_polarity_iterative
+    examples = [
+      {
+        input: "aA",
+        expected: ""
+      },
+      {
+        input: "abBA",
+        expected: ""
+      },
+      {
+        input: "abAB",
+        expected: "abAB"
+      },
+      {
+        input: "aabAAB",
+        expected: "aabAAB"
+      },
+      {
+        input: "dabAcCaCBAcCcaDA",
+        expected: "dabCBAcaDA"
+      }
+    ]
+
+    examples.each do |example|
+      assert_equal example[:expected], resolve_polarity_iterative(example[:input])
     end
   end
 end
