eeb3730ae5dcdeaf1d2c0ac8eb3e18a04a36df27
Katherine Wu <kwugirl@gmail.com>
2018-12-13 17:19:49 -0500

    13A: Add Carts to map

--- day_13.rb
+++ day_13.rb
@@ -1,22 +1,70 @@
 class Map
+  attr_reader :carts
+
   def initialize(input)
     @grid = {}
+    @carts = []
 
     input.each_with_index do |row, y|
       row.each_char.with_index do |char, x|
-        case char
-        when "^", "v"
-          char = "|"
-        when "<", ">"
-          char = "-"
-        end
+        location = "#{x}, #{y}"
 
-        @grid["#{x}, #{y}"] = char
+        @grid["#{x}, #{y}"] = case char
+        when "^"
+          {
+            track: "|",
+            cart: new_cart(location, :up)
+          }
+        when "v"
+          {
+            track: "|",
+            cart: new_cart(location, :down)
+          }
+        when "<"
+          {
+            track: "-",
+            cart: new_cart(location, :left)
+          }
+        when ">"
+          {
+            track: "-",
+            cart: new_cart(location, :right)
+          }
+        else
+          {
+            track: char
+          }
+        end
       end
     end
   end
 
-  def location(x, y)
+  def at(x, y)
     @grid["#{x}, #{y}"]
+  end
+
+  def track_at(x, y)
+    @grid["#{x}, #{y}"][:track]
+  end
+
+  def cart_at(x, y)
+    @grid["#{x}, #{y}"][:cart]
+  end
+
+  private
+
+  def new_cart(location, direction)
+    cart = Cart.new(location, direction)
+    @carts << cart
+    cart
+  end
+end
+
+class Cart
+  attr_accessor :location, :direction
+
+  def initialize(location, direction)
+    @location = location
+    @direction = direction
   end
 end
